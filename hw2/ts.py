89800c87c09aa7a46a12c9203d8eabef27c031f7
kuzzmak <kuzminski.antonio@gmail.com>
2021-12-14 21:29:37 +0100

    Added code for result generation, tabu search now receives initial solution.

--- hw2/ts.py
+++ hw2/ts.py
@@ -1,17 +1,22 @@
 import random
 from typing import List
 
-from common import read_file, Player, POSITION, Solution
+import matplotlib.pyplot as plt
+
+from common import read_file, Player, POSITION, Solution, random_solution
 from greedy import greedy
+from local_search import local_search
+from validator import Validator
+
 
 
 class TS:
 
-    def __init__(self, players: List[Player], budget: float):
+    def __init__(self, players: List[Player], budget: float, tabu_tenure: int):
         self._players = players
         self._budget = budget
         self._n_neighbors = 15
-        self._tabu_tenure = 3
+        self._tabu_tenure = tabu_tenure
         self._tabu = [0] * self._n_neighbors
         self._players_by_position = {
             POSITION.MID: [p for p in players if p.position == POSITION.MID],
@@ -64,12 +69,9 @@ class TS:
             if self._tabu[i] > 0:
                 self._tabu[i] -= 1
 
-    def run(self) -> Solution:
-        current_solution = greedy(self._players, self._budget)
-        while not current_solution.valid():
-            current_solution = greedy(self._players, self._budget)
-        best_solution = current_solution
-
+    def run(self, initial_solution: Solution) -> Solution:
+        current_solution = initial_solution
+        best_solution = initial_solution
         for _ in range(self._n_neighbors + 1):
             self._decrement_tabu()
             neighbors = self._get_neighbors(current_solution)
@@ -91,9 +93,65 @@ class TS:
 
 
 if __name__ == '__main__':
-    instance_path = r'C:\Users\tonkec\Documents\hmu\hw2\instance1.txt'
-    players = read_file(instance_path)
+    instance = 'instance1'
     budget = 100
-    ts = TS(players, budget)
-    solution = ts.run()
-    solution.save('ts.txt')
+    instance_path = fr'C:\Users\tonkec\Documents\hmu\hw2\data\{instance}.txt'
+    players = read_file(instance_path)
+
+    for i in range(9):
+
+        initial_solution = random_solution(players)
+        # initial_solution = greedy(players, budget)
+        # while not initial_solution.valid():
+        #     initial_solution = greedy(players, budget)
+
+        ts = TS(players, budget, 3)
+        solution = ts.run(initial_solution)
+        solution.save('ts.txt')
+        solution = local_search(players, budget, 20, initial_solution)
+        solution.save('ls.txt')
+
+        print('exp ', str(i))
+        price, points = Validator(instance_path, 'ts.txt').validate()
+        print('ts points: ', points)
+        price, points = Validator(instance_path, 'ls.txt').validate()
+        print('ls points: ', points)
+        print()
+
+    # num_of_runs = 100
+    # for instance in ['instance1', 'instance2']:
+    #     instance_path = fr'C:\Users\tonkec\Documents\hmu\hw2\data\{instance}.txt'
+    #     players = read_file(instance_path)
+    #     for tenure in [3, 5, 7]:
+    #         for rand in [True, False]:
+    #             ts = TS(players, budget, tenure, rand)
+    #             best = 0
+    #             best_solution = None
+    #             x = []
+    #             y = []
+    #             for i in range(num_of_runs):
+    #                 if rand:
+    #                     initial_solution = random_solution(players)
+    #                 else:
+    #                     initial_solution = greedy(players, budget)
+    #                     while not initial_solution.valid():
+    #                         initial_solution = greedy(players, budget)
+    #                 solution = ts.run(initial_solution)
+    #                 solution.save('ts.txt')
+    #                 price, points = Validator(
+    #                     instance_path,
+    #                     'ts.txt',
+    #                 ).validate()
+    #                 x.append(i)
+    #                 y.append(points)
+    #                 if points >= best:
+    #                     best = points
+    #                     best_solution = solution
+
+    #             solution_type = 'random_initial' if rand else 'greedy'
+    #             save_string = f'{instance}_ts_{solution_type}_{best}_tabu_tenure_{tenure}'
+    #             best_solution.save(save_string + '.txt')
+
+    #             plt.plot(x, y, 'xr')
+    #             plt.savefig(save_string + '.png')
+    #             plt.clf()
