58103ac6866e2757c5c1e5b8636d2030483788f4
kuzzmak <kuzminski.antonio@gmail.com>
2022-01-01 21:38:52 +0100

    Not ceiling result anymore.

--- project/utils.py
+++ project/utils.py
@@ -3,5 +3,5 @@ import math
 from customer import Customer
 
 
-def calculate_distance(c1: Customer, c2: Customer) -> int:
-    return math.ceil(math.sqrt((c1.x - c2.x)**2 + (c1.y - c2.y)**2))
+def calculate_distance(c1: Customer, c2: Customer) -> float:
+    return math.sqrt((c1.x - c2.x)**2 + (c1.y - c2.y)**2)
