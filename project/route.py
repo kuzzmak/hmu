8efcea462fc33271ac4da8c75a1c12e94b263322
kuzzmak <kuzminski.antonio@gmail.com>
2022-01-01 21:37:55 +0100

    Implemented __eq__ method, added real length property which tracks distance without ceiling every distance between customers.

--- project/route.py
+++ project/route.py
@@ -1,3 +1,4 @@
+import math
 from typing import List
 
 from customer import Customer
@@ -11,8 +12,10 @@ class Route:
         self._capacity = capacity
         self._customers = [self._base]
         self._length = 0
+        self._real_length = 0
         self._demand = 0
         self._time = 0
+        self._times = [self._time]
 
     @property
     def customers(self) -> List[Customer]:
@@ -22,11 +25,21 @@ class Route:
     def time(self) -> int:
         return self._time
 
+    @property
+    def length(self) -> int:
+        return self._length
+
+    @property
+    def real_length(self) -> float:
+        return self._real_length
+
     def can_add_customer(self, customer: Customer):
-        dist = calculate_distance(self._customers[-1], customer)
+        dist = math.ceil(calculate_distance(self._customers[-1], customer))
         new_time = self._time + dist
+        if new_time < customer.ready_t:
+            new_time = customer.ready_t
         time_constraint_customer = new_time <= customer.due_t
-        dist = calculate_distance(self._base, customer)
+        dist = math.ceil(calculate_distance(self._base, customer))
         time_constraint_base = new_time + dist <= self._base.due_t
         demand_constraint = self._demand + customer.demand <= self._capacity
         return time_constraint_customer and \
@@ -35,14 +48,28 @@ class Route:
 
     def add_customer(self, customer: Customer) -> None:
         dist = calculate_distance(self._customers[-1], customer)
-        self._time = self._time + dist + customer.service_t
+        self._real_length += dist
+        self._length += math.ceil(dist)
+        self._time = max(self._time + math.ceil(dist), customer.ready_t) + \
+            customer.service_t
+        self._times.append(self._time - customer.service_t)
         self._demand += customer.demand
         self._customers.append(customer)
 
     def __str__(self) -> str:
         s = ''
         for i, c in enumerate(self.customers):
-            s += str(c.no)
+            s += str(c.no) + f'({self._times[i]})'
             if i < len(self.customers) - 1:
                 s += '->'
         return s
+
+    def __eq__(self, other: "Route") -> bool:
+        if self._length != other._length:
+            return False
+        if self.time != other._time:
+            return False
+        for c1, c2 in zip(self.customers, other.customers):
+            if c1 != c2:
+                return False
+        return True
