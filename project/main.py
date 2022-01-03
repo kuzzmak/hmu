0e0543766271210e8c95c784bafa4b26456cd614
kuzzmak <kuzminski.antonio@gmail.com>
2022-01-03 01:12:23 +0100

    Implemented project.

--- project/main.py
+++ project/main.py
@@ -1,4 +1,5 @@
 import random
+import time
 from typing import List, Tuple
 
 import numpy as np
@@ -127,25 +128,51 @@ def read_instance(instance_path: str) -> Tuple[int, int, List[Customer]]:
 
 
 def merge_routes(r1: Route, r2: Route) -> List[Route]:
-    base = r1.customers[0]
-    customers = [*r1.customers[1:], *r2.customers[1:]]
-    new_routes = []
-    r1_new = make_route(base, customers, r1._capacity)
-    new_routes.append(r1_new)
-    for c in r1_new.customers:
-        if c.no == base.no:
-            continue
-        customers.remove(c)
-    if len(customers) == 0:
-        return new_routes
-    r2_new = make_route(base, customers, r1._capacity)
-    new_routes.append(r2_new)
+    tries = 0
+    while True:
+        if tries >= 100:
+            return [r1, r2]
+        base = r1.customers[0]
+        customers = [*r1.customers[1:-1], *r2.customers[1:-1]]
+        new_routes = []
+        r1_new = make_route(base, customers, r1._capacity)
+        r1_new.add_customer(base)
+        new_routes.append(r1_new)
+        for c in r1_new.customers:
+            if c.no == base.no:
+                continue
+            customers.remove(c)
+        if len(customers) == 0:
+            return new_routes
+        r2_new = make_route(base, customers, r1._capacity)
+        r2_new.add_customer(base)
+        new_routes.append(r2_new)
+        if len(r1.customers) + len(r2.customers) == len(r1_new.customers) + len(r2_new.customers):
+            break
+        else:
+            tries += 1
     return new_routes
 
 
-def random_swap_two_customers(solution: Solution) -> Solution:
+def random_merge_two_routes(solution: Solution) -> Solution:
+    routes = [*solution.routes]
+    r1 = random.choice(routes)
+    r2 = random.choice(routes)
+    while r1 == r2:
+        r2 = random.choice(routes)
+    routes.remove(r1)
+    routes.remove(r2)
+    new_routes = merge_routes(r1, r2)
+    routes = [*new_routes, *routes]
+    s = Solution()
+    [s.add_route(r) for r in routes]
+    return s
+
+
+def random_swap_two_customers(solution: Solution, capacity: int) -> Solution:
     base = solution.routes[0].customers[0]
     ok = True
+    new_solution = Solution()
     while True:
         r1 = random.choice(solution.routes)
         r2 = random.choice(solution.routes)
@@ -171,113 +198,74 @@ def random_swap_two_customers(solution: Solution) -> Solution:
         cs_2.append(r_c_1)
         new_r_1 = make_route(base, cs_1, capacity)
         new_r_1.add_customer(base)
-        if len(r1.customers) == len(new_r_1.customers):
-            ok = True
-        else:
-            ok = False
         new_r_2 = make_route(base, cs_2, capacity)
         new_r_2.add_customer(base)
-        if len(r2.customers) == len(new_r_2.customers):
-            ok = ok & True
-        else:
-            ok = False
+        ok = len(r1.customers) + len(r2.customers) == \
+            len(new_r_1.customers) + len(new_r_2.customers)
         if ok:
-            solution.routes.remove(r1)
-            solution.routes.remove(r2)
-            solution.add_route(new_r_1)
-            solution.add_route(new_r_2)
+            routes = [*solution.routes]
+            routes.remove(r1)
+            routes.remove(r2)
+            routes.append(new_r_1)
+            routes.append(new_r_2)
+            solution = Solution()
+            [new_solution.add_route(r) for r in routes]
             break
-    return solution
+    return new_solution
+
+
+def make_population(
+    population_size: int,
+    veh_num: int,
+    capacity: int,
+    customers: List[Customer],
+) -> List[Solution]:
+    pop = [greedy(veh_num, capacity, customers)
+           for _ in range(population_size)]
+    return pop
+
+
+def run(instance_path: str, population_size: int, seconds: int) -> Solution:
+    veh_num, capacity, customers = read_instance(instance_path)
+    population = make_population(population_size, veh_num, capacity, customers)
+    population = sorted(population, key=lambda s: (s.vehicles, s.length))
+    best = population[0]
+    t = time.time()
+    time_string = '{:>3}/{} seconds elapsed - '
+    while time.time() - t < seconds:
+        new_pop = [*population[:population_size // 2]]
+        while len(new_pop) < population_size and time.time() - t < seconds:
+            rnd_sol = random.choice(new_pop)
+            s1 = random_swap_two_customers(rnd_sol, capacity)
+            s2 = random_merge_two_routes(rnd_sol)
+            s3 = random_merge_two_routes(s2)
+            new_s = sorted(
+                [s1, s2, s3],
+                key=lambda s: (s.vehicles, s.length),
+            )
+            best_new_s = new_s[0]
+            if best_new_s.better_than(rnd_sol):
+                new_pop.append(best_new_s)
+                if best_new_s.better_than(best):
+                    best = best_new_s
+                    print(
+                        time_string.format(round(time.time() - t), seconds),
+                        f'new best - routes: {best.vehicles}, length: {best.length}',
+                    )
+        print(time_string.format(round(time.time() - t), seconds))
+        population = sorted(new_pop, key=lambda s: (s.vehicles, s.length))
+    return best
 
 
 if __name__ == '__main__':
-    instance_path = r'C:\Users\tonkec\Documents\hmu\project\data\i1.txt'
-    veh_num, capacity, customers = read_instance(instance_path)
-    solution = greedy(veh_num, capacity, customers)
-    # print(solution)
-    # print()
-    solution = random_swap_two_customers(solution)
-    # print(solution)
-    solution.save()
+    for i in range(6):
+        instance = f'i{i+1}'
+        instance_path = fr'C:\Users\tonkec\Documents\hmu\project\data\{instance}.txt'
+        population_size = 10
+        for seconds, t in [(60, '1m'), (300, '5m'), (600, 'un')]:
+            solution = run(instance_path, population_size, seconds)
+            save_string = f'res-{t}m-{instance}.txt'
+            solution.save(save_string)
 
-    # r1 = random.choice(solution.routes)
-    # r2 = random.choice(solution.routes)
-    # while r1 == r2:
-    #     r2 = random.choice(solution.routes)
-    # cs_1 = r1.customers[1:-1]
-    # cs_2 = r2.customers[1:-1]
-    # # print(cs_1)
-    # rnd_idx_1 = random.randint(1, len(cs_1) - 1)
-    # rnd_idx_2 = random.randint(1, len(cs_2) - 1)
-    # r_c_1 = cs_1[rnd_idx_1]
-    # r_c_2 = cs_2[rnd_idx_2]
-
-    # cs_1.remove(r_c_1)
-    # cs_2.remove(r_c_2)
-    # cs_1.append(r_c_2)
-    # cs_2.append(r_c_1)
-
-    # new_r_1 = make_route(customers[0], cs_1, capacity)
-    # new_r_2 = make_route(customers[0], cs_2, capacity)
-
-    # print('sel 1', r_c_1)
-    # print('sel 2', r_c_2)
-    # print()
-    # print('old 1', r1)
-    # print('new 1', new_r_1)
-    # print()
-    # print('old 2', r2)
-    # print('new 2', new_r_2)
-
-    # solution.save()
-    # r1 = random.choice(solution.routes)
-    # r2 = random.choice(solution.routes)
-    # while r1 == r2:
-    #     r2 = random.choice(solution.routes)
-    # new_routes = merge_routes(r1, r2)
-    # r3 = make_route(customers[0], [*r1.customers[1:],
-    #                 *r2.customers[1:]], capacity)
-    # print(r1)
-    # print(r2)
-    # for r in new_routes:
-    #     print(r)
-
-    # draw_customers(customers[1:])
-    # draw_base(customers[0])
-
-    # r1 = solution.routes[0]
-    # r2 = solution.routes[1]
-    # draw_route(r1)
-    # draw_route(r2)
-    # plt.show()
-
-    # by_dist = sorted(customers[1:], key=lambda c: calculate_distance(c, customers[0]))
-    # print(by_dist[0])
-    # a = list(map(lambda c: c.ready_t, customers))
-    # bins = 10
-    # hist = np.histogram(a, bins)
-    # std_dev = np.std(hist[0])
-    # print(std_dev)
-    # print(hist)
-
-    # print(solution)
-    # draw_customers(customers[1:])
-    # draw_base(customers[0])
-    # for r in routes:
-    #     draw_route(r)
-    # plt.show()
-    # # base
-    # c1 = Customer(0, 0, 0, 1, 0, 999, 0)
-    # # (0, 1)
-    # c2 = Customer(0, 1, 0, 1, 0, 999, 2)
-    # # (1, 2)
-    # c3 = Customer(0, 1, 2, 1, 0, 999, 3)
-    # # (2, 0)
-    # c4 = Customer(0, 0, 2, 1, 0, 999, 4)
-
-    # r = Route(c1, 999)
-    # r.add_customer(c2)
-    # r.add_customer(c3)
-    # r.add_customer(c4)
-    # print(r._times)
-    # print(r.length)
+    # veh_num, capacity, customers = read_instance(instance_path)
+    # solution = greedy(veh_num, capacity, customers)
